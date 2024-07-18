d28a7be47965147656faf9f554875580e21de74e
조우식 <chousik01@gmail.com>
2024-07-18 16:46:13 +0900

    feat | LiComponent.js
    컴포넌트 내부에서 동작하는 for of 문을 제거

--- skeleton/client/components/LiComponent.js
+++ skeleton/client/components/LiComponent.js
@@ -1,13 +1,5 @@
 import { TagComponent } from "./TagComponent.js";
 
-export const LiComponent = (incommingData)=>{
-
-   let liString = ""
-
-  for(let element of incommingData){
-
-    liString += TagComponent("li",`제목 : ${element.title}, 저자 : ${element.author}`)
-
-  }
-  return liString
+export const LiComponent = (textNode)=>{
+  return TagComponent("li", textNode)
 }
