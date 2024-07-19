4cea354304edd765763ab229e1c1a0efc6aa5f03
조우식 <chousik01@gmail.com>
2024-07-19 17:41:34 +0900

    feat | index.js
    제작된 모듈 결합

--- skeleton/client/public/js/index.js
+++ skeleton/client/public/js/index.js
@@ -1,5 +1,8 @@
 import { Assembly } from "./components/Assembly.js"
 import KeydownEvenet from "./module/KeydownEvent.js"
+import ClickEvent from "./module/ClickEvent.js"
 
 
 KeydownEvenet("Enter", "button")
+
+ClickEvent("button")
