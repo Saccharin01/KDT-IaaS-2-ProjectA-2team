0d83dcbfc2fb911b5ace1da8e2ea0630f920fa19
jaemin1005 <ghkdwoals1005@gmail.com>
2024-07-19 14:20:13 +0900

    New | mongoDB.js : mongoDB 접속을 위한 Class
    생성자 함수 매개변수와 메서드를 이용하여
    DB 접속, 스키마 생성, 스키마 가져오기를 할 수 있음

---
+++ skeleton/DB/modules/MongoDB/mongoDB.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+
+class MongoDB {
+
+  /**
+   * * 생성자 함수를 통해 몽고 DB에 접속합니다. 
+   * @param {*} serverUrl : 서버 주소
+   */
+  constructor(serverUrl) {
+    this.serverUrl = serverUrl;
+    this.schemaMap = new Map();
+  }
+
+  async Connect() {
+    mongoose.connect(this.serverUrl, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    const db = mongoose.connection;
+    db.on('error', console.error.bind(console, 'connection error:'));
+    db.once('open', function () {
+      console.log('Connected to the database!');
+    });
+  }
+
+  //* schema에 해당 데이터베이스 삽입.
+  InsertSchema(schemaName, schemaDefinition) {
+    const { Schema, model, models } = mongoose;
+
+    const schema = new Schema(schemaDefinition);
+
+    //* 이미 존재하는 스키마가 있을 경우 기존 꺼 사용, 아닐 경우 새로 생성합니다.
+    const value = models[schemaName] || model(schemaName, schema);
+
+    this.schemaMap.set(schemaName, value);
+  }
+
+  //* scehma 가져오기
+  GetSchema(schemaName) {
+    if (this.schemaMap.has(schemaName) === false) throw new Error(`Don't have key: ${schemaName}`);
+    return this.schemaMap.get(schemaName);
+  }
+}
+
+module.exports = MongoDB
