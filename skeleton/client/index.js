38b9b62b1e57505fb3a9922a4e692b50468a2dfb
jaemin1005 <ghkdwoals1005@gmail.com>
2024-07-19 16:31:42 +0900

    Rename | index.js, Assembly.js LiComponent.js, TagComponent.js, UlComponent.js KeydownEvent.js

--- skeleton/client/index.js
+++
@@ -1,5 +0,0 @@
-import { Assembly } from "../client/components/Assembly.js"
-import KeydownEvenet from "../client/module/KeydownEvent.js"
-
-
-KeydownEvenet("Enter", "button")
---
+++ skeleton/client/public/js/index.js
@@ -0,0 +1,5 @@
+import { Assembly } from "./components/Assembly.js"
+import KeydownEvenet from "./module/KeydownEvent.js"
+
+
+KeydownEvenet("Enter", "button")
