03de8600657115a1fbfa4109a00ddfbe292f040c
jaemin1005 <ghkdwoals1005@gmail.com>
2024-07-19 16:42:06 +0900

    Fix | server/main.js : 포트 변경

--- skeleton/server/main.js
+++ skeleton/server/main.js
@@ -1,5 +1,5 @@
 const app = require('./app.js');
-const PORT = process.env.SERVER_PORT || 3000;
+const PORT = process.env.SERVER_PORT || 3001;
 
 app.listen(PORT, () => {
   console.log(`Server Open : http://localhost:${PORT}`);
