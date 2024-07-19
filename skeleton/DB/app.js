2471f6da66a895ce797372679d57652498572fad
jaemin1005 <ghkdwoals1005@gmail.com>
2024-07-19 17:21:11 +0900

    Feat | DB/app.js : cors 오류 해결을 위해 미들웨어 추가

--- skeleton/DB/app.js
+++ skeleton/DB/app.js
@@ -2,7 +2,9 @@ const express = require('express');
 const searctrt = require("./routers/searchrt.js");
 const MongoDB = require('./modules/MongoDB/mongoDB.js');
 const app = express();
+const cors = require('cors');
 
+app.use(cors());
 app.use("/search", searctrt);
 
 module.exports = app;
