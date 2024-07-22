91d6d30bcb4af492431fa36e00775e9e5352c2d4
jaemin1005 <ghkdwoals1005@gmail.com>
2024-07-22 16:30:03 +0900

    Feat | app.module.ts : 환경변수 이용을 위해서 ConfigModule을 import 하였음

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProxyModule } from './proxy/proxy.module';
+import { ConfigModule } from '@nestjs/config';
+import { configOptions } from '@shared/config/env.config';
 
 @Module({
-  imports: [],
+  imports: [ProxyModule, ConfigModule.forRoot(configOptions)],
   controllers: [AppController],
   providers: [AppService],
 })
