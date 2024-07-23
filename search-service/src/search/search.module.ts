ef0e0e1895f50bf4f83c3ebd7e8ae13d098d31a8
조우식 <chousik01@gmail.com>
2024-07-23 19:53:54 +0900

    feat | search.module.ts
    데이터베이스 환경변수 처리 수정

--- search-service/src/search/search.module.ts
+++ search-service/src/search/search.module.ts
@@ -6,10 +6,12 @@ import { MongooseModule } from '@nestjs/mongoose';
 import * as dotEnv from 'dotenv'
 dotEnv.config()
 
+const test = process.env.MONGO_URL
+
 // const url = "http://localhost:27017/"
 @Module({
   imports: [
-    MongooseModule.forRoot(process.env.MONGO_URL)
+    MongooseModule.forRoot(test)
   ],
   controllers : [SearchController],
   providers : [SearchService],
