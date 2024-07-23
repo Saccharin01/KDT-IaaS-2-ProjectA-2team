51cd909b80871ca57af997e8034431c26c50f9e5
조우식 <chousik01@gmail.com>
2024-07-23 18:51:00 +0900

    오류 발생 로직 빌드중

--- search-service/src/search/search.service.ts
+++ search-service/src/search/search.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from "@nestjs/common";
 // import mongoose from "mongoose";
 // import { BookSchema } from "../modules/Schema";
-import { SearchDTO, SearchResponse } from "@shared/SearchDTO";
-import { Model } from 'src/modules/Model';
+import { SearchDTO, SearchResponse , BookDTO} from "@shared/SearchDTO";
+import { BookModel } from 'src/modules/Model';
 
 
 // todo 몽구스에서 제공하는 메서드를 이용해봐야 함.
@@ -17,9 +17,9 @@ export class SearchService{
     const searchRegex = new RegExp(query, 'i'); // 대소문자 구분 없이 검색
     
     try {
-      const data = await Model.find({ title: { $regex: searchRegex } });
+      const data = await BookModel.find({ title: { $regex: searchRegex } }).exec();
       console.log('Found data:', data);
-      return data;
+      return {incomeData: data};
     } catch (err) {
       console.error('Error fetching books:', err);
       throw err; // 에러 발생 시 프로미스를 reject 상태로 만듭니다.
