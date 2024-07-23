001afedcb6889f54fb75de5e8cd1ddcce92c572e
조우식 <chousik01@gmail.com>
2024-07-23 18:50:27 +0900

    feat | search.module.ts
    모듈 파일 엔트리 설정
    환경 변수를 이용해 URL의 익명화 및 DB연결 처리

--- search-service/src/search/search.module.ts
+++ search-service/src/search/search.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { SearchController } from './search.controller';
 import { SearchService } from './search.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
+import * as dotEnv from 'dotenv'
+dotEnv.config()
+
+// const url = "http://localhost:27017/"
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(process.env.MONGO_URL)
+  ],
   controllers : [SearchController],
   providers : [SearchService],
 })
