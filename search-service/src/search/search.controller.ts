5b560bcd69ada29afb46e93dc9a351be7e4f7bba
조우식 <chousik01@gmail.com>
2024-07-23 15:56:06 +0900

    반환되는 프로미스를 체이닝을 이용해 fulfill로 바꿔 반환

--- search-service/src/search/search.controller.ts
+++ search-service/src/search/search.controller.ts
@@ -1,17 +1,19 @@
 import { Controller, Get, Query } from '@nestjs/common';
 import { SearchService } from './search.service';
+import { SearchDTO } from "@shared/SearchDTO"
+
+
+
 
 @Controller('search')
 export class SearchController {
   constructor(private readonly searchService: SearchService) {}
 
-
-
-
   // ? /search/books?{someQuery} = {UserInputQuery}
   
   @Get('books')
-  SearchResult(@Query('someQuery') query: string) {
-    return this.searchService.searchBooks(query);
+  Result(@Query('title') query: string) {
+    this.searchService.searchBooks(query)
+    .then(data => data)
   }
 }
