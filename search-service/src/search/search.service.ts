7b5d0cfddcf39a554e613693c1f01a564b706ab4
Saccharin01 <chousik01@gmail.com>
2024-07-25 03:50:41 +0900

    feat | search.servcie.ts
    컨트롤러에서 처리 한 엔드포인트에 대한 비즈니스 로직 정의

--- search-service/src/search/search.service.ts
+++ search-service/src/search/search.service.ts
@@ -1,14 +1,26 @@
-import { Injectable } from "@nestjs/common";
-
-// todo 몽구스에서 제공하는 메서드를 이용해봐야 함.
-// todo search 서비스 상 데이터베이스에서 특정 정보를 검색 해보오는 로직은 많이 쓰일 것. 그렇다면 해당 부분을 모듈로 분리해야 할 것 같음.
-
-
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { SearchResponse, BookDTO } from '@shared/SearchDTO';
+import { Model } from 'mongoose';
 
 @Injectable()
-export class SearchService{
-  searchBooks(query : string){
+export class SearchService {
+  constructor(
+    @InjectModel('book_info') private readonly bookModel: Model<BookDTO>,
+  ) {}
 
-    return "test"
-  }
+  searchBooks = async (query: string): Promise<SearchResponse> => {
+    const searchRegex = new RegExp(query, 'i'); // 대소문자 구분 없이 검색
+
+    try {
+      const data = await this.bookModel
+        .find({ title: { $regex: searchRegex } })
+        .exec();
+      console.log('Found data:', data);
+      return { incomeData: data };
+    } catch (err) {
+      console.error('Error fetching books:', err);
+      throw err; // 에러 발생 시 프로미스를 reject 상태로 만듭니다.
+    }
+  };
 }
