e4eeda31128e43afbd4d981917c7363004e27d7a
조우식 <chousik01@gmail.com>
2024-07-24 20:47:52 +0900

    내부 로직 수정

--- search-service/src/search/search.service.ts
+++ search-service/src/search/search.service.ts
@@ -1,23 +1,26 @@
 import { Injectable } from "@nestjs/common";
-// import mongoose from "mongoose";
-// import { BookSchema } from "../modules/Schema";
+import { InjectModel } from "@nestjs/mongoose";
 import { SearchDTO, SearchResponse , BookDTO} from "@shared/SearchDTO";
-import { BookModel } from 'src/modules/Model';
-
+// import { BookModel } from 'src/modules/Model';
+import { Model } from "mongoose";
+import { BookSchema } from "src/modules/Schema";
+import { searchInterface } from "./SearchService.interface";
 
 // todo 몽구스에서 제공하는 메서드를 이용해봐야 함.
 // todo search 서비스 상 데이터베이스에서 특정 정보를 검색 해보오는 로직은 많이 쓰일 것. 그렇다면 해당 부분을 모듈로 분리해야 할 것 같음.
-
+const modelName = process.env.MODEL_NAME
 
 @Injectable()
 export class SearchService{
-  
-  searchBooks = async (query: string): Promise<SearchResponse> => {
-    // const model = mongoose.model(modelName, BookSchema, modelName);
+  constructor(@InjectModel("book_info") private readonly bookModel: Model<BookDTO>) {}
+
+
+
+  searchBooks = async(query: string): Promise<SearchResponse> => {
     const searchRegex = new RegExp(query, 'i'); // 대소문자 구분 없이 검색
     
     try {
-      const data = await BookModel.find({ title: { $regex: searchRegex } }).exec();
+      const data = await this.bookModel.find({ title: { $regex: searchRegex } }).exec();
       console.log('Found data:', data);
       return {incomeData: data};
     } catch (err) {
