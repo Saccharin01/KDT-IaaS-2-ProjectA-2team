0ffd1b6040a79b345c3b54be8f751e10dc5f21f5
Saccharin01 <chousik01@gmail.com>
2024-07-25 03:49:23 +0900

    feat | app.module.ts
    검색 서비스의 모듈 주입

--- search-service/src/app.module.ts
+++ search-service/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SearchModule } from './search/search.module';
+import { ConfigModule } from '@nestjs/config';
+import { configOptions } from '@shared/config/env.config';
 
 @Module({
-  imports: [],
+  imports: [SearchModule, ConfigModule.forRoot(configOptions)],
   controllers: [AppController],
   providers: [AppService],
 })
