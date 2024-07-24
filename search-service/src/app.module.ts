8be3ca7341993d843edccdc155b4eaec0555406d
조우식 <chousik01@gmail.com>
2024-07-24 20:47:17 +0900

    반성문

--- search-service/src/app.module.ts
+++ search-service/src/app.module.ts
@@ -2,10 +2,36 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SearchModule } from './search/search.module';
+import { ConfigModule } from '@nestjs/config';
+import { configOptions } from '@shared/config/env.config';
 
 @Module({
-  imports: [SearchModule],
+  imports: [SearchModule, ConfigModule.forRoot(configOptions)],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// @Module({
+//   imports: [SearchModule, ConfigModule.forRoot(configOptions)],
+//   controllers: [AppController],
+//   providers: [AppService],
+// })
+// export class AppModule {}
