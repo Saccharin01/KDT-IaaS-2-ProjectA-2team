c2169cc48fd0cd1a2f8e72149e2d4817362d4c72
조우식 <chousik01@gmail.com>
2024-07-24 21:03:32 +0900

    feat | Model.ts
    불필요해 보이는거 없에는데, 이것도 사실 테스트거든?? 그래서 잘 되는지 잘
    안되는지 확인해야해

--- search-service/src/modules/Model.ts
+++ search-service/src/modules/Model.ts
@@ -2,8 +2,8 @@ import mongoose from 'mongoose'
 import { BookSchema } from './Schema'
 import * as dotenv from "dotenv"
 
-dotenv.config()
-const name = process.env.MODEL_NAME
+// dotenv.config()
+// const name = process.env.MODEL_NAME
 
 
-export const BookModel = mongoose.model(name,BookSchema)
+// export const BookModel = mongoose.model(name,BookSchema)
