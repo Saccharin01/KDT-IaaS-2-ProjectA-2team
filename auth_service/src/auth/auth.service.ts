d861d10ecc8956d2ebdbc37a8b3f5c3c54e3733a
jaemin1005 <ghkdwoals1005@gmail.com>
2024-07-25 18:01:48 +0900

    New | auth.service.ts : _id를 통하여 검증하는 서비스
    validateUserById
    _id를 이용하여, moongose를 통해 옳바른 접근인지 확인한다.
    _id는 jwt토큰으로, 디코딩된 매개변수:

---
+++ auth_service/src/auth/auth.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User, UserDocument } from './schemas/user.schema';
+import { JwtPayloadDTO } from '@shared/dto/JwtPayloadDTO';
+
+@Injectable()
+export class AuthService {
+  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+
+  /**
+   * * _id를 통해 유저 정보를 접근한다.
+   * @param _id
+   * @returns
+   */
+  async validateUserById(_id: string): Promise<JwtPayloadDTO | null> {
+    const user = await this.userModel
+      .findById(_id)
+      .select('-password -__v')
+      .lean();
+    if (!user) {
+      return null;
+    }
+    return user;
+  }
+}
