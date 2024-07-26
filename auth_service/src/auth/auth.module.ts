35d886095295b43cc008767cf863cb3c8c51ae28
jaemin1005 <ghkdwoals1005@gmail.com>
2024-07-26 17:38:25 +0900

    New | auth.module.ts : 인증 모듈

---
+++ auth_service/src/auth/auth.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtModuleOptions } from './config/jwt.config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { JwtStrategy } from './jwt.strategy';
+
+@Module({
+  imports: [
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_URI'),
+      }),
+      inject: [ConfigService],
+    }),
+    PassportModule,
+    JwtModule.register(jwtModuleOptions),
+    ConfigModule.forRoot(),
+  ],
+  providers: [AuthService, JwtStrategy],
+})
+export class AuthModule {}
