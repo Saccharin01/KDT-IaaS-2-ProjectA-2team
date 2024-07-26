6d61d414fdd18c8152def0024de4ba05eb453819
jaemin1005 <ghkdwoals1005@gmail.com>
2024-07-26 17:37:17 +0900

    Fix | auth.service.ts : import 경로 수정

--- auth_service/src/auth/auth.service.ts
+++ auth_service/src/auth/auth.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User, UserDocument } from './schemas/user.schema';
-import { JwtPayloadDTO } from '@shared/dto/JwtPayloadDTO';
+import { JwtPayloadDTO } from './dto/JwtPayloadDTO';
 
 @Injectable()
 export class AuthService {
