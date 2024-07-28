b959673ee56cd40ee15039c8f8d4a1a91938d0a1
jaemin1005 <ghkdwoals1005@gmail.com>
2024-07-28 21:54:29 +0900

    Feat | auth_service/app.controller.ts : 로그인, 회원가입, 아이디중복검사 기능 추가

--- auth_service/src/app.controller.ts
+++ auth_service/src/app.controller.ts
@@ -1,12 +1,48 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  UnauthorizedException,
+  ConflictException,
+  Get,
+  Query,
+} from '@nestjs/common';
+import LoginDTO from '@shared/dto/loginDTO';
+import CheckIdDTO from '@shared/interfaces/checkId';
+import SuccessDTO from '@shared/dto/success.dto';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Post('login')
+  async login(@Body() loginDto: LoginDTO) {
+    const user = await this.appService.validateUser(
+      loginDto._id,
+      loginDto.password,
+    );
+    if (!user) {
+      throw new UnauthorizedException('InCorrect UserId or Password');
+    }
+
+    const token = await this.appService.makeJWT(loginDto);
+    return token;
+  }
+
+  // 회원가입 엔드포인트
+  @Post('signup')
+  async signup(@Body() body: LoginDTO) {
+    const result = await this.appService.create(body); // 사용자 생성
+
+    if (result === false) throw new ConflictException('User already exists');
+
+    return;
+  }
+
+  @Get('checkId')
+  async checkId(@Query() query: CheckIdDTO): Promise<SuccessDTO> {
+    const result = await this.appService.checkDuplicate(query.userId);
+    return { success: result };
   }
 }
