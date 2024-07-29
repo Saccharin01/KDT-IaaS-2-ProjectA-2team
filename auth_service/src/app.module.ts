ffa06f4994f5583ea28dcc4481d719c06696462c
jaemin1005 <ghkdwoals1005@gmail.com>
2024-07-29 12:50:10 +0900

    Feat | auth_service/app.module.ts : ConfigModule과 Mongoose Module import

--- auth_service/src/app.module.ts
+++ auth_service/src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtModuleOptions } from '@shared/config/jwt.config';
+import { configOptions } from '@shared/config/env.config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from '@shared/schemas/user.schema';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(configOptions),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('DATABASE_HOST'),
+      }),
+      inject: [ConfigService],
+    }),
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    JwtModule.register(jwtModuleOptions),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
