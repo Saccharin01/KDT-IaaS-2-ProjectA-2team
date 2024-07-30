e983aa114fc7072f33d9861208b7e2f6150b4a8d
조우식 <chousik01@gmail.com>
2024-07-30 19:55:12 +0900

    로직 재 정의

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,10 +1,21 @@
-import ExplainComponent from "frontend/components/ExplainationComponent/ExplainationComponent";
 import data from "../components/testData";
+import PageAssembly from "frontend/components/ImageComponent/PageAssembly";
+import ImageAssembly from "frontend/components/ImageComponent/ImageAssembly";
+import ExplanationAssembly from "frontend/components/ExplainationComponent/ExplainationAssembly";
 
+
+// const searchPage = () => {
+//   return (
+//     <div id="root">
+//       <ImageAssembly parsedData={data.parsedData}/>
+//       <ExplanationAssembly parsedData={data.parsedData} />
+//     </div>
+//   );
+// };
 const searchPage = () => {
   return (
     <div id="root">
-      <ExplainComponent parsedData={data.parsedData}/>
+      <PageAssembly/>
     </div>
   );
 };
