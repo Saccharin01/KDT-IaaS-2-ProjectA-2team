90fd4a5a1d5cbfa3cd462640603735bffb8d2bc2
조우식 <chousik01@gmail.com>
2024-07-30 15:40:09 +0900

    feat | SearchDTO.ts
    DTO 확장성을 위해 구조  수정

--- shared/SearchDTO.ts
+++ shared/SearchDTO.ts
@@ -1,22 +1,35 @@
 import { Document } from "mongoose";
 
 
+ interface base {
+  _id : number;
+  title : string;
+  author? : string;
+  price: number;
+  genre? : string;
+  publisher? : string;
+  explanation? : string;
+  stock : number;  
+}
 
-export interface BookDTO extends Document {
-    _id : number;
-    title : string;
-    author? : string;
-    price: number;
-    genre? : string;
-    publisher? : string;
-    explanation? : string;
-    stock : number;
+
+
+
+export interface BookDTO extends Document{
+  _id : number;
+  title : string;
+  author? : string;
+  price: number;
+  genre? : string;
+  publisher? : string;
+  explanation? : string;
+  stock : number;  
 }
 
 export interface SearchResponse {
   incomeData: BookDTO[]
 }
 
-export default SearchResponse
+export default base
 
 
