35551acc232340e453229d8fdadc192a0c83d6b1
조우식 <chousik01@gmail.com>
2024-07-30 19:54:21 +0900

    image 컴포넌트 인터페이스 정의

--- frontend/components/ImageComponent/ImageComponent.tsx
+++ frontend/components/ImageComponent/ImageComponent.tsx
@@ -1,14 +1,7 @@
 import IImageInterface from "./IImageInterface";
 
-const ImageComponent: React.FC<IImageInterface> = ({parsedData}) => {
-
-  return(
-      parsedData.map((element, index)=>
-      <div className="produce_image" key={index}>
-        <img src={element._id.toString()} alt={element._id.toString()}/>
-      </div>
-    )
-  )
+const ImageComponent: React.FC<IImageInterface> = ({ ...props}) => {
+  return <img {...props} />
 }
 
 export default ImageComponent
