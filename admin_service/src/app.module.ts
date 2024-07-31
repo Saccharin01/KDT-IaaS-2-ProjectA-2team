a04c36a652986d1781be317473e39a834e8146e0
황재민 <ghkdwoals1005@gmail.com>
2024-07-31 12:57:32 +0900

    Feat | admin_service/app.module.ts : Mongoose 모듈 추가

--- admin_service/src/app.module.ts
+++ admin_service/src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BooksModule } from './books/books.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { configOptions } from '@shared/config/env.config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(configOptions),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('DATABASE_HOST'),
+      }),
+      inject: [ConfigService],
+    }),
+    BooksModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
