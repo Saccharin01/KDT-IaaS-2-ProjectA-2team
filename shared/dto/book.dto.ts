748ae2653ecdc2531bdf5032589870a75c495557
황재민 <ghkdwoals1005@gmail.com>
2024-07-31 13:02:33 +0900

    New | shared/book.dto.ts : 책정보에 대한 dto

---
+++ shared/dto/book.dto.ts
@@ -0,0 +1,11 @@
+export interface BookDto {
+  _id: number;
+  title: string;
+  author: string;
+  price: number;
+  genre: string;
+  publisher: string;
+  stock: number;
+  explanation: string;
+  out: boolean;
+}
