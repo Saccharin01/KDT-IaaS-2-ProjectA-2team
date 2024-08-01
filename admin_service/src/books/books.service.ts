12299548a13c4bb7e6249737bd67522f805d9d11
황재민 <ghkdwoals1005@gmail.com>
2024-08-01 23:05:13 +0900

    Feat | admin/books.service.ts : book_info 컬렉션에 데이터를 수정하는 메소드 추가

--- admin_service/src/books/books.service.ts
+++ admin_service/src/books/books.service.ts
@@ -1,8 +1,14 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  BadRequestException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Book, BookDocument } from '@shared/schemas/book.schema';
 import { Model } from 'mongoose';
 import { BookDto } from '@shared/dto/book.dto';
+import mongoose from 'mongoose';
 
 @Injectable()
 export class BooksService {
@@ -12,5 +18,31 @@ export class BooksService {
 
   async getBooks(): Promise<BookDto[]> {
     return await this.bookModel.find().select('-__v').lean();
+  }
+
+  async updateBoook(bookDto: BookDto): Promise<BookDto> {
+    const { _id, ...anotherField } = bookDto;
+    try {
+      const updatedBook = await this.bookModel
+        .findByIdAndUpdate(_id, anotherField, {
+          new: true,
+          runValidators: true,
+        })
+        .select('-__v')
+        .lean();
+
+      if (!updatedBook) {
+        throw new NotFoundException(`Book with ID ${_id} not found`);
+      }
+
+      return updatedBook;
+    } catch (error) {
+      if (error instanceof mongoose.Error.ValidationError) {
+        // 데이터 유효성 검사 실패 시 처리
+        throw new BadRequestException('Invalid data format');
+      }
+      // 그 외의 에러 처리
+      throw new InternalServerErrorException('An unexpected error occurred');
+    }
   }
 }
