1229de858b9d0d195607a156192f7e7bad16db66
조우식 <chousik01@gmail.com>
2024-08-01 18:01:32 +0900

    new | About.tsx
    상세 페이지 랜더링 정의

---
+++ frontend/pages/search/About.tsx
@@ -0,0 +1,13 @@
+import data from "../../components/testData";
+import DetailPageAssembly from "frontend/components/Assembly/DetailPage/DetailPageAssembly";
+
+
+const DetailPage = () => {
+  return (
+    <div id="root">
+      <DetailPageAssembly data={data.test}/>
+    </div>
+  );
+};
+
+export default DetailPage
