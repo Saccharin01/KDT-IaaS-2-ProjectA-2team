09bb3aca18cdc57889e0efb9929cbf8f6ba93ff6
조우식 <chousik01@gmail.com>
2024-08-01 17:46:59 +0900

    feat | Schema.ts
    스키마 중복으로 인한 미 사용 스키마 파일 주석처리

--- search-service/src/modules/Schema.ts
+++ search-service/src/modules/Schema.ts
@@ -1,14 +1,14 @@
-import { BookDTO } from '@shared/SearchDTO';
-import { Schema } from 'mongoose';
+// import { BookDTO } from '@shared/SearchDTO';
+// import { Schema } from 'mongoose';
 
 
-export const BookSchema = new Schema<BookDTO>({
-  _id: { type: Number, required: true },
-  title: { type: String, required: true },
-  author: { type: String},
-  publisher: { type: String},
-  genre: { type: String},
-  price: { type: Number, required: true },
-  explanation: { type: String},
-  stock: { type: Number, required: true },
-});
+// export const BookSchema = new Schema<BookDTO>({
+//   _id: { type: Number, required: true },
+//   title: { type: String, required: true },
+//   author: { type: String},
+//   publisher: { type: String},
+//   genre: { type: String},
+//   price: { type: Number, required: true },
+//   explanation: { type: String},
+//   stock: { type: Number, required: true },
+// });
