b3c88ad9beae03024424611665cb967e15cb623c
황재민 <ghkdwoals1005@gmail.com>
2024-08-01 23:12:48 +0900

    Test | Feat | books.service.spec.ts
    book.service.ts 테스트
    1. updateBoook() 메소드 테스트
    업데이트 성공적으로 이루어지는지 반환값을 통해 검사

--- admin_service/src/books/__test__/books.service.spec.ts
+++ admin_service/src/books/__test__/books.service.spec.ts
@@ -4,57 +4,12 @@ import { BooksService } from '../books.service';
 import { MongooseModule, getModelToken } from '@nestjs/mongoose';
 import { Book, BookDocument, BookSchema } from '@shared/schemas/book.schema';
 import mongoose, { Model } from 'mongoose';
-import { BookDto } from '@shared/dto/book.dto';
+import { bookArrayData } from './dummybooks.spec';
 
 describe('BooksService', () => {
   let service: BooksService;
   let mongodb: MongoMemoryServer;
   let bookModel: Model<BookDocument>;
-
-  const bookArrayData: BookDto[] = [
-    {
-      _id: 1,
-      title: 'The Great Gatsby',
-      author: 'F. Scott Fitzgerald',
-      price: 10.99,
-      genre: 'Fiction',
-      publisher: 'Scribner',
-      stock_quantity: 100,
-      introduce: 'A novel set in the Jazz Age on Long Island.',
-      remain_stock: 100,
-      sold_stock: 0,
-      arrival_date: new Date('2024-07-23T00:00:00Z'),
-      hashtags: 'init',
-    },
-    {
-      _id: 2,
-      title: '1984',
-      author: 'George Orwell',
-      price: 8.99,
-      genre: 'Dystopian',
-      publisher: 'Secker & Warburg',
-      stock_quantity: 50,
-      introduce: 'A novel about a dystopian future under totalitarian rule.',
-      remain_stock: 50,
-      sold_stock: 0,
-      arrival_date: new Date('2024-07-23T00:00:00Z'),
-      hashtags: 'bye',
-    },
-    {
-      _id: 3,
-      title: 'To Kill a Mockingbird',
-      author: 'Harper Lee',
-      price: 12.99,
-      genre: 'Southern Gothic',
-      hashtags: '#안녕',
-      publisher: 'J.B. Lippincott & Co.',
-      stock_quantity: 200,
-      introduce: 'A novel about racial injustice in the Deep South.',
-      remain_stock: 200,
-      sold_stock: 0,
-      arrival_date: new Date('2024-07-23T00:00:00Z'),
-    },
-  ];
 
   beforeAll(async () => {
     mongodb = await MongoMemoryServer.create();
@@ -87,5 +42,27 @@ describe('BooksService', () => {
     const getData = await service.getBooks();
     expect(getData.length).toBe(3);
     expect(getData).toEqual(bookArrayData);
+  });
+
+  describe('데이터 업데이트 하기', () => {
+    it('성공적으로 업데이트', async () => {
+      const updateData = {
+        _id: 3,
+        title: 'Update',
+        author: 'Harper Lee',
+        price: 12.99,
+        genre: 'Southern Gothic',
+        hashtags: '#안녕',
+        publisher: 'J.B. Lippincott & Co.',
+        stock_quantity: 200,
+        introduce: 'A novel about racial injustice in the Deep South.',
+        remain_stock: 200,
+        sold_stock: 0,
+        arrival_date: new Date('2024-07-23T00:00:00Z'),
+      };
+
+      const result = await service.updateBoook(updateData);
+      expect(result).toEqual(updateData);
+    });
   });
 });
