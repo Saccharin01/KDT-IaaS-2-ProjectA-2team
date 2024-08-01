f1ba397aac6ecbb725231861fe4bb722acf9f10e
황재민 <ghkdwoals1005@gmail.com>
2024-08-01 23:50:40 +0900

    Test | admin_service/books.controller.spec.ts
    books.controller.ts에 대한 테스트입니다.
    1. Get으로 요청했을 시 모든 데이터를 반환하는지 확인합니다
    2. Put 요청 했을 시 업데이트가 제대로 이루어지는지 확인합니다
    3. Put 존재하지 않는 데이터에 업데이트 했을 시 에러를 반환하는지 확인합니다
    4. Put 업데이트 유효성 검사 실패시 에러를 반환하는지 확인합니다

---
+++ admin_service/src/books/__test__/books.controller.spec.ts
@@ -0,0 +1,94 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { BooksController } from '../books.controller';
+import supertest from 'supertest';
+import { INestApplication } from '@nestjs/common';
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import { Book, BookDocument, BookSchema } from '@shared/schemas/book.schema';
+import { MongooseModule, getModelToken } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { bookArrayData, updateBoook } from './dummybooks.spec';
+import mongoose from 'mongoose';
+import { BooksService } from '../books.service';
+
+describe('BooksController', () => {
+  let controller: BooksController;
+  let app: INestApplication;
+  let mongodb: MongoMemoryServer;
+  let bookModel: Model<BookDocument>;
+
+  beforeAll(async () => {
+    mongodb = await MongoMemoryServer.create();
+    const url = mongodb.getUri();
+
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        MongooseModule.forRoot(url),
+        MongooseModule.forFeature([{ name: Book.name, schema: BookSchema }]),
+      ],
+      controllers: [BooksController],
+      providers: [BooksService],
+    }).compile();
+
+    app = module.createNestApplication();
+    app.init();
+    controller = module.get<BooksController>(BooksController);
+    bookModel = module.get<Model<BookDocument>>(getModelToken(Book.name));
+
+    await bookModel.create(bookArrayData);
+  });
+
+  afterAll(async () => {
+    await mongoose.disconnect();
+    await mongodb.stop();
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+
+  it('Get Method Test', async () => {
+    const response = await supertest(app.getHttpServer()).get('/books');
+    expect(response.body.length).toBe(3);
+
+    //! 응답받을 때, 문자열로 된 날짜를 다시 파싱해야한다.
+    response.body.forEach((element) => {
+      element.arrival_date = new Date(element.arrival_date);
+    });
+
+    expect(response.body).toEqual(bookArrayData);
+  });
+
+  describe('Put Method Test', () => {
+    it('Success', async () => {
+      const response = await supertest(app.getHttpServer())
+        .put('/books')
+        .send(updateBoook);
+
+      response.body.arrival_date = new Date(response.body.arrival_date);
+
+      expect(response.body).toEqual(updateBoook);
+    });
+
+    it('존재하지 않는 Id', async () => {
+      const copy = structuredClone(updateBoook);
+      copy._id = 4;
+
+      const response = await supertest(app.getHttpServer())
+        .put('/books')
+        .send(copy);
+
+      expect(response.status).toBe(500);
+    });
+
+    it('유효성 검사 실패', async () => {
+      const copy = structuredClone(updateBoook);
+      copy.price = -1;
+
+      const response = await supertest(app.getHttpServer())
+        .put('/books')
+        .send(copy);
+
+      expect(response.status).toBe(400);
+    });
+  });
+});
