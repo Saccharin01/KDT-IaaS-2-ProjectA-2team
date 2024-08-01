8b5afb841531f2f8fedfe158a1d0b923442264ad
Saccharin01 <chousik01@gmail.com>
2024-08-02 03:16:10 +0900

    feat | SearchDTO.ts
    내부에 정의된 인터페이스 분리

--- shared/SearchDTO.ts
+++ shared/SearchDTO.ts
@@ -1,24 +1,5 @@
 import { Document } from "mongoose";
 
-
- interface base {
-  _id : number;
-  title : string;
-  author? : string;
-  price: number;
-  genre? : string;
-  publisher? : string;
-  introduce? : string;
-  hashtags: string;
-  arrival_date?: number;
-  stock_quantity : number;
-  Remain_stock?: number
-  Sold_stock?: number
-}
-
-
-
-
 export interface BookDTO extends Document{
   _id : number;
   title : string;
@@ -37,8 +18,6 @@ export interface BookDTO extends Document{
 export interface SearchResponse {
   incomeData: BookDTO[]
 }
-
-export default base
 
 
 
