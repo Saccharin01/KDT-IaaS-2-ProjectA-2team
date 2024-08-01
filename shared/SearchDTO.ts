8deb0f6d17f02af1162462b4a01aed4ccb14c56e
조우식 <chousik01@gmail.com>
2024-08-01 17:47:56 +0900

    feat | SearchDTO.ts
    변경된 DTO에 대한 수정

--- shared/SearchDTO.ts
+++ shared/SearchDTO.ts
@@ -1,18 +1,21 @@
 import { Document } from "mongoose";
 
 export interface BookDTO extends Document {
-    _id : number;
-    title : string;
-    author? : string;
-    price: number;
-    genre? : string;
-    publisher? : string;
-    explanation? : string;
-    stock : number;
+  _id: number;
+  title: string;
+  author: string;
+  price: number;
+  genre: string;
+  publisher: string;
+  stock_quantity: number; // 필드 이름 수정
+  introduce: string; // 필드 이름 수정
+  hashtags: string; // 추가된 필드
+  arrival_date: Date; // 추가된 필드
+  remain_stock: number; // 추가된 필드
+  sold_stock: number; // 
 }
 
 export interface SearchResponse {
   incomeData: BookDTO[]
 }
-
 
