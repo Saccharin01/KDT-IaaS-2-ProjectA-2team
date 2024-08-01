9df57eaad281483901429e7d8410072427f3f7ec
조우식 <chousik01@gmail.com>
2024-08-01 17:46:23 +0900

    feat | search.module.ts
    리터럴 값 제거

--- search-service/src/search/search.module.ts
+++ search-service/src/search/search.module.ts
@@ -2,14 +2,14 @@ import { Module } from '@nestjs/common';
 import { SearchController } from './search.controller';
 import { SearchService } from './search.service';
 import { MongooseModule } from '@nestjs/mongoose';
-import { BookSchema } from 'src/modules/Schema';
+import { Book,BookSchema } from '@shared/schemas/book.schema';
 import { ConfigModule } from '@nestjs/config';
 import { configOptions } from '@shared/config/env.config';
 
 @Module({
   imports: [
     ConfigModule.forRoot(configOptions),
-    MongooseModule.forFeature([{ name: 'book_info', schema: BookSchema }]),
+    MongooseModule.forFeature([{ name: Book.name, schema: BookSchema }]),
     MongooseModule.forRoot(process.env.DATABASE_HOST),
   ],
   controllers: [SearchController],
