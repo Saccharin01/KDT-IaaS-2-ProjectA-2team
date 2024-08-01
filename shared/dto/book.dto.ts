e5929d01be7b4e7bce78b8e1af90d1567a28d32e
황재민 <ghkdwoals1005@gmail.com>
2024-08-01 09:49:04 +0900

    Fix | shard/book.dto.ts : MongoDB의 컬렉션에 맞게, dto 수정

--- shared/dto/book.dto.ts
+++ shared/dto/book.dto.ts
@@ -5,7 +5,10 @@ export interface BookDto {
   price: number;
   genre: string;
   publisher: string;
-  stock: number;
-  explanation: string;
-  out: boolean;
+  stock_quantity: number; // 필드 이름 수정
+  introduce: string; // 필드 이름 수정
+  hashtags: string; // 추가된 필드
+  arrival_date: Date; // 추가된 필드
+  remain_stock: number; // 추가된 필드
+  sold_stock: number; // 추가된 필드
 }
