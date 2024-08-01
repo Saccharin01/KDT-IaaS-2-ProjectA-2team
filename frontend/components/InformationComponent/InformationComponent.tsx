d2c1e1c98d0b7de1d9e03e33712479e05ad73a6c
조우식 <chousik01@gmail.com>
2024-08-01 18:03:08 +0900

    new | informationComponent.tsx
    검색 결과 페이지의 하위 컴포넌트 정의

---
+++ frontend/components/InformationComponent/InformationComponent.tsx
@@ -0,0 +1,42 @@
+import IDetailInterface from "./IInformationInterface"
+import ParameterComponent from "../ParameterComponent/ParameterComponent";
+
+
+const InformationComponent: React.FC<IDetailInterface> = ({title,author,price,explanation}) => {
+  return (
+    <div>
+          <div>
+            <h3>{title}</h3>
+          </div>
+          <div>
+            <h4>{author}</h4>
+          </div>
+          <div>
+            <ParameterComponent>{price}</ParameterComponent>
+          </div>
+          <div>
+            <ParameterComponent>{explanation}</ParameterComponent>
+          </div>
+    </div>
+  );
+};
+export default InformationComponent;
+
+
+
+
+// const DetailComponent: React.FC<IExplainationInterface> = ({parsedData}) => {
+
+//   return ( 
+
+
+//     <div className="details" key={index}>
+//       <h3>{element.title}</h3>
+//       <h4>{element.author}</h4>
+//       <ParameterComponent>{element.price}원</ParameterComponent>
+//       <ParameterComponent>{element.explanation}</ParameterComponent>
+//     </div>
+        
+//   );
+// };
+
