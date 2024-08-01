3a2d46ca59b928aee8804789ae1352885b06916c
Saccharin01 <chousik01@gmail.com>
2024-08-02 03:24:07 +0900

    feat | search.controller.ts
    컨트롤러 경로 처리 추가. url 뒤에 id가 붙어오는 경우, 해당 값을 숫자형으로 변경 후 서비스 로직에 넘깁니다.

--- search-service/src/search/search.controller.ts
+++ search-service/src/search/search.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Query, Param} from '@nestjs/common';
 import { SearchService } from './search.service';
 /**
  * 프록시 서버에서 라우팅 된 로직을 분배하는 컨트롤러
@@ -6,12 +6,22 @@ import { SearchService } from './search.service';
  * 쿼리스트링의 key인 title이 하드 코딩 되어있기 때문에 해당 부분에 대한
  * 방법 강구 필요.
  */
-@Controller()
+
+@Controller('books')
 export class SearchController {
   constructor(private readonly searchService: SearchService) {}
+  
+  //* books/122012032192
+  @Get(':id')
+  searchBookById(@Param('id') id: string) {
+    // ID를 숫자로 변환
+    const parsedNum = Number(id)
+    return this.searchService.searchBooksById(parsedNum);
+    }
 
-  @Get('books')
-  getHello(@Query('title') query: string) {
+  @Get()
+  searchBookByTitle(@Query('title') query: string) {
     return this.searchService.searchBooks(query);
   }
+
 }
