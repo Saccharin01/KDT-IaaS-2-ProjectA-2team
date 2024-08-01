680964ff8928b35c76ea7bce7ed136d932b50dd3
Saccharin01 <chousik01@gmail.com>
2024-08-02 03:39:34 +0900

    feat | SearchBooksAssemblyInterface.ts
    인터페이스 분리에 의한 수정

--- frontend/components/Assembly/SearchBooks/SearchBooksAssemblyInterface.ts
+++ frontend/components/Assembly/SearchBooks/SearchBooksAssemblyInterface.ts
@@ -1,9 +1,15 @@
-import base from "@shared/SearchDTO";
+import Ititle from "@shared/interfaces/ITitle"
+import IPrice from "@shared/interfaces/IPrice"
+import IHashtag from "@shared/interfaces/IHashtag"
+import IIntroduce from "@shared/interfaces/IInrtoduce"
+import IStockQuantity from "@shared/interfaces/IStockQuantity"
+import IAuthor from "@shared/interfaces/IAuthor"
+import IId from "@shared/interfaces/IId"
 
-interface basePlate extends base {}
+interface data extends IHashtag,IIntroduce,Ititle,IPrice,IStockQuantity,IAuthor,IId{}
 
 interface ISearchBooksInterface{
-  data : basePlate[]
+  data : data[]
 }
 
 
