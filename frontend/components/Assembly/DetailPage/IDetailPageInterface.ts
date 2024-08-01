e564a63017b831da678286353d1c16f3dbaebccd
Saccharin01 <chousik01@gmail.com>
2024-08-02 03:38:22 +0900

    feat | IDetailPageInterface.ts
    인터페이스 분리에 의한 수정

--- frontend/components/Assembly/DetailPage/IDetailPageInterface.ts
+++ frontend/components/Assembly/DetailPage/IDetailPageInterface.ts
@@ -1,6 +1,10 @@
-import base from "@shared/SearchDTO";
+import Ititle from "@shared/interfaces/ITitle"
+import IPrice from "@shared/interfaces/IPrice"
+import IHashtag from "@shared/interfaces/IHashtag"
+import IIntroduce from "@shared/interfaces/IInrtoduce"
+import IStockQuantity from "@shared/interfaces/IStockQuantity"
 
-interface data extends base {}
+interface data extends IHashtag,IIntroduce,Ititle,IPrice,IStockQuantity {}
 
 interface IDetailPageInterface {
   data : data[]
