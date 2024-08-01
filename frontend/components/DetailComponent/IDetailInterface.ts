9fd4c1772f59395d01cda4948a63390ac4b8571e
조우식 <chousik01@gmail.com>
2024-08-01 18:21:41 +0900

    new | IDetailinterface.ts
    검색 정보 하위 컴포넌트 인터페이스 정의

---
+++ frontend/components/DetailComponent/IDetailInterface.ts
@@ -0,0 +1,12 @@
+import Title from"@shared/interfaces/ITitle"
+import Author from"@shared/interfaces/IAuthor"
+import Price from"@shared/interfaces/IPrice"
+import Explanation from"@shared/interfaces/IExplanation"
+
+
+interface IDetailInterface extends Title, Author, Price, Explanation{
+  className? : string
+}
+
+
+export default IDetailInterface
