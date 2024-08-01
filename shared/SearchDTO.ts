47f5aa52326025afb72e66382190ea1e69fbc4db
조우식 <chousik01@gmail.com>
2024-08-01 17:58:55 +0900

    feat | SearchDTO.ts

--- shared/SearchDTO.ts
+++ shared/SearchDTO.ts
@@ -1,21 +1,44 @@
 import { Document } from "mongoose";
 
-export interface BookDTO extends Document {
-  _id: number;
-  title: string;
-  author: string;
+
+ interface base {
+  _id : number;
+  title : string;
+  author? : string;
   price: number;
-  genre: string;
-  publisher: string;
-  stock_quantity: number; // 필드 이름 수정
-  introduce: string; // 필드 이름 수정
-  hashtags: string; // 추가된 필드
-  arrival_date: Date; // 추가된 필드
-  remain_stock: number; // 추가된 필드
-  sold_stock: number; // 
+  genre? : string;
+  publisher? : string;
+  introduce? : string;
+  hashtags: string;
+  arrival_date?: number;
+  stock_quantity : number;
+  Remain_stock?: number
+  Sold_stock?: number
+}
+
+
+
+
+export interface BookDTO extends Document{
+  _id : number;
+  title : string;
+  author? : string;
+  price: number;
+  genre? : string;
+  publisher? : string;
+  introduce? : string;
+  hashtags: string;
+  arrival_date?: number;
+  stock_quantity : number;
+  Remain_stock?: number
+  Sold_stock?: number
 }
 
 export interface SearchResponse {
   incomeData: BookDTO[]
 }
+
+export default base
+
+
 
