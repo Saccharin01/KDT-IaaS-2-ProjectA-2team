1810220f494416b531846b4a9f29b44ff75f1222
조우식 <chousik01@gmail.com>
2024-08-02 15:39:30 +0900

    comment | index.tsx
    데이터 페칭 관련 주의사항 추가

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -9,7 +9,13 @@ const testState = ()=>{
   console.log(state)
 
   useEffect(()=>{
-    fetch("http://localhost:3000/search/books?title=하나님")
+    /**
+     * ! 프록시 경로처리 로직에 주의
+     * ! /search/books/2407190 같은 경우 프록시 서버에서 숫자 부분을 "경로"로 처리해버림
+     * ! data 페칭 시 "/" 붙이면 안됨
+     * ! 쿼리스트링으로 요청을 보내야 하는 경우 꼼꼼하게 확인해서 처리해야 함.
+     */
+    fetch("http://localhost:3000/search/books 2407190")
     .then((res)=>res.json())
     .then((data)=>setState(data))
     .catch((err)=>{console.error(`fetch failed: ${err}`)})
