ba1227d0fc8ee89fb4aa00395ac4739872dd5ca6
조우식 <chousik01@gmail.com>
2024-08-02 12:44:34 +0900

    feat | index.tsx
    데이터 패칭 훅 테스트

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,13 +1,39 @@
+import { useEffect, useState } from "react";
 import data from "../components/testData";
 import SearchBooksAssembly from "frontend/components/Assembly/SearchBooks/SearchBooksAssembly";
 
-const SearchBook = () => {
+useState
+
+const testState = ()=>{
+  const [state,setState] = useState(null)
+  console.log(state)
+
+  useEffect(()=>{
+    fetch("http://localhost:3000/search/books?title=하나님")
+    .then((res)=>res.json())
+    .then((data)=>setState(data))
+    .catch((err)=>{console.error(`fetch failed: ${err}`)})
+  },[])
+
+  {console.log(state)}
+
   return (
     <div id="root"
     className="h-screen">
-      <SearchBooksAssembly data={data.test}/>
+      {state ? <SearchBooksAssembly data={state.incomeData}/> : <p>wtf</p>}
+      {/* <SearchBooksAssembly data={data.test}/> */}
     </div>
   );
-};
+}
 
-export default SearchBook
+
+// const SearchBook = () => {
+//   return (
+//     <div id="root"
+//     className="h-screen">
+//       <SearchBooksAssembly data={data.test}/>
+//     </div>
+//   );
+// };
+
+export default testState
