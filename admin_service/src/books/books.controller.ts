d303b29878768d82401d7d63da96a2368c6d9963
황재민 <ghkdwoals1005@gmail.com>
2024-08-02 16:03:55 +0900

    Feat | admin_service/books.controller.ts : http Post Method 처리 메소드 추가
    유효성 검사후 bookservice의 createBook메소드를 통해, 컬렉션에 데이터를 저장한다

--- admin_service/src/books/books.controller.ts
+++ admin_service/src/books/books.controller.ts
@@ -1,4 +1,11 @@
-import { Body, Controller, Get, ValidationPipe, Put } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  ValidationPipe,
+  Put,
+  Post,
+} from '@nestjs/common';
 import { BooksService } from './books.service';
 import { BookDto } from '@shared/dto/book.dto';
 
@@ -12,9 +19,17 @@ export class BooksController {
   }
 
   @Put()
-  updateBooks(
+  updateBook(
     @Body(new ValidationPipe({ forbidNonWhitelisted: true })) bookDto: BookDto,
   ) {
     return this.bookService.updateBook(bookDto);
+  }
+
+  @Post()
+  createBook(
+    @Body(new ValidationPipe({ forbidNonWhitelisted: true }))
+    bookDto: Omit<BookDto, '_id'>,
+  ) {
+    return this.bookService.createBook(bookDto);
   }
 }
