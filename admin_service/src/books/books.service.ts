0375e1a3e1ca864cb779a95f6ad0680cd2c95366
황재민 <ghkdwoals1005@gmail.com>
2024-08-02 12:04:02 +0900

    Feat, Fix | admin_service/books.service.ts : createBook 컬렉션에 데이터를 추가하는 메소드 구현
    updateBook : BookDto의 날짜의 문자열을 파악하여, Date형식으로 변경하는 로직 추가
    createBook : arrival_date의 문자열을 파악, Date타입으로 변경 후 컬렉션에 데이터를 추가한다.
    반환되는 값은 pick을 이용하여 Dto에 해당되는 키만 가져와 반환한다.

--- admin_service/src/books/books.service.ts
+++ admin_service/src/books/books.service.ts
@@ -9,6 +9,7 @@ import { Book, BookDocument } from '@shared/schemas/book.schema';
 import { Model } from 'mongoose';
 import { BookDto } from '@shared/dto/book.dto';
 import mongoose from 'mongoose';
+import { pick } from 'lodash';
 
 @Injectable()
 export class BooksService {
@@ -16,18 +17,30 @@ export class BooksService {
     @InjectModel(Book.name) private readonly bookModel: Model<BookDocument>,
   ) {}
 
-  async getBooks(): Promise<BookDto[]> {
+  async getBooks() {
     return await this.bookModel.find().select('-__v').lean();
   }
 
-  async updateBook(bookDto: BookDto): Promise<BookDto> {
-    const { _id, ...anotherField } = bookDto;
+  async updateBook(bookDto: BookDto) {
+    const { _id, arrival_date, ...anotherField } = bookDto;
+    let date: Date;
+
+    if (arrival_date === '') {
+      date = new Date();
+    } else {
+      date = new Date(arrival_date);
+    }
+
     try {
       const updatedBook = await this.bookModel
-        .findByIdAndUpdate(_id, anotherField, {
-          new: true,
-          runValidators: true,
-        })
+        .findByIdAndUpdate(
+          _id,
+          { arrival_date: date, ...anotherField },
+          {
+            new: true,
+            runValidators: true,
+          },
+        )
         .select('-__v')
         .lean();
 
@@ -39,10 +52,42 @@ export class BooksService {
     } catch (error) {
       if (error instanceof mongoose.Error.ValidationError) {
         // 데이터 유효성 검사 실패 시 처리
-        throw new BadRequestException('Invalid data format');
+        throw new BadRequestException(
+          'Validation failed: ' + JSON.stringify(error.errors),
+        );
       }
       // 그 외의 에러 처리
       throw new InternalServerErrorException('An unexpected error occurred');
+    }
+  }
+
+  async createBook(bookDto: BookDto) {
+    const { _id, arrival_date, ...anotherField } = bookDto;
+    let date: Date;
+    if (arrival_date === '') {
+      date = new Date();
+    } else {
+      date = new Date(arrival_date);
+    }
+
+    const bookModel = await this.bookModel.create({
+      _id,
+      arrival_date: date,
+      ...anotherField,
+    });
+
+    try {
+      const savedBook = await bookModel.save();
+      return pick(savedBook, Object.keys(bookDto));
+    } catch (error) {
+      //* 유효성 검사가 실패
+      if (error instanceof mongoose.Error.ValidationError) {
+        throw new BadRequestException(
+          'Validation failed: ' + JSON.stringify(error.errors),
+        );
+      } else {
+        throw new BadRequestException('An error occurred while saving');
+      }
     }
   }
 }
