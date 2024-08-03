554ca4e2256fad30e8c76f3dc93b238950595b18
황재민 <ghkdwoals1005@gmail.com>
2024-08-03 15:04:02 +0900

    Style | shared/book.dto.ts : 주석 제거

--- shared/dto/book.dto.ts
+++ shared/dto/book.dto.ts
@@ -5,10 +5,10 @@ export interface BookDto {
   price: number;
   genre: string;
   publisher: string;
-  stock_quantity: number; // 필드 이름 수정
-  introduce: string; // 필드 이름 수정
-  hashtags: string; // 추가된 필드
-  arrival_date: string; // 추가된 필드
-  remain_stock: number; // 추가된 필드
-  sold_stock: number; // 추가된 필드
+  stock_quantity: number;
+  introduce: string;
+  hashtags: string; 
+  arrival_date: string; 
+  remain_stock: number; 
+  sold_stock: number; 
 }
