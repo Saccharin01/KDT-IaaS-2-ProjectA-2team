d65e39338c18e1d254c0498bd5cd9606d71833f8
조우식2 <chousik01@gmail.com>
2024-08-05 12:40:41 +0900

    test input Filed

--- frontend/src/app/input/page.tsx
+++ frontend/src/app/input/page.tsx
@@ -1,12 +1,65 @@
-import React from "react";
+'use client'
 
-const Input = ()=>{
+import React from "react";
+import { useState } from "react";
+
+    
+    
+    
+    
+
+const Input:React.FC = ()=>{
+
+    const [value,setValue] = useState<string>('')
+
+    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => { 
+        console.log(value)
+         setValue(event.target.value); }
+
+
+    const ClickHandler = ()=>{
+
+        fetch("http://localhost:3001/input",{
+        method:"POST",
+        body:value,
+        headers : {
+        'Content-Type' : "text/plain"
+        }
+    })
+    .then((res)=>{
+        if(res.ok){
+            console.log("submit success")
+            return res.json()
+        } else{
+            console.log("submit failed")
+            return new Error()
+        }
+    })
+    .then((data)=>{
+        console.log(`received : ${data}`)
+    })
+}
+
+
+
+
   return(
-    <input type="text" placeholder="Hello,world!"
-    className="border-2">
-    </input>
+    
+    <div>
+        <input type="text" placeholder="Hello,world!" className="border-2 mb-7" onChange={handleInputChange}></input>
+        <div className="
+            border-2 
+            bg-red-400 h-8 w-20 flex flex-col items-center cursor-pointer hover:bg-red-800"
+            onClick={ClickHandler}
+            >button</div>
+        <div>result section</div>
+
+
+
+    </div>
   )
 }
+
 
 
 
