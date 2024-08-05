f263880bfb34084eca84a9d369b7d6eebde72779
조우식2 <chousik01@gmail.com>
2024-08-05 17:33:17 +0900

    feat | app.service.ts

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -5,4 +5,11 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+  receivePostRequest(parsedData : string):{response : string}[] {
+    console.log(parsedData)
+    return [
+      {response : `hello! i'm Nest.js! this is what we got "${parsedData}"`}
+    ]
+  }
 }
+
