5e512d34617e1625dc79fa30916000be83653618
조우식 <chousik01@gmail.com>
2024-08-05 21:15:39 +0900

    feat | DataBaseSetting.ts

--- shared/dataBaseSetting.ts
+++ shared/dataBaseSetting.ts
@@ -1,14 +1,4 @@
-import * as mongoose from 'mongoose';
 export const DataBaseSetting = {
   url: 'mongodb+srv://chousik01:70318202@cluster0.mmiiheq.mongodb.net/',
   name: 'testDataBase',
 };
-
-// Test 인터페이스 정의
-export interface IName extends Document {
-  name: string;
-}
-
-export const TestSchema = new mongoose.Schema<IName>({
-  name: { type: String, required: true },
-});
