bf0d5dea6d9d5e16819841c8882ddbef179b5343
조우식 <chousik01@gmail.com>
2024-08-05 21:12:53 +0900

    feat | app.module.ts
    의존성 주입

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,12 +3,15 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { DataBaseSetting, TestSchema} from '../../shared/dataBaseSetting';
+import {Input, InputSchema} from "@shared/input.schema"
+const url= 'mongodb+srv://chousik01:70318202@cluster0.mmiiheq.mongodb.net/'
+
+
 
 @Module({
   imports: [
-    MongooseModule.forRoot(DataBaseSetting.url),
-    MongooseModule.forFeature([{ name: DataBaseSetting.name, schema: TestSchema }]),
+    MongooseModule.forRoot(url),
+    MongooseModule.forFeature([{ name: Input.name, schema: InputSchema }]),
   ],
   controllers: [AppController],
   providers: [AppService],
