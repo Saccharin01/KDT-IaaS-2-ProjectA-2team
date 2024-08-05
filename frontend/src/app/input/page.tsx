b3bb70ba127796ed6ed4ccf01c1b693941af0f05
조우식2 <chousik01@gmail.com>
2024-08-05 11:31:55 +0900

    input Page

---
+++ frontend/src/app/input/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Input = ()=>{
+  return(
+    <input type="text" placeholder="Hello,world!"
+    className="border-2">
+    </input>
+  )
+}
+
+
+
+export default Input
