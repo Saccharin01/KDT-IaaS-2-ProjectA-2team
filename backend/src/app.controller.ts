6be34ea9a227282b7e3db9ba6aefa4964c42ebf0
조우식2 <chousik01@gmail.com>
2024-08-05 17:32:59 +0900

    feat | app.controller.ts

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +8,12 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post('/input')
+  receivePostRequest(@Body() body : { data : string }){
+    const {data} = body
+    console.log(data)
+    return this.appService.receivePostRequest(data)
   }
 }
