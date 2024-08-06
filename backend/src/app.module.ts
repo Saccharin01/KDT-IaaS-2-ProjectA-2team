a77d15e795b7c45dcbbb5df95f3e0105b000bacd
조우식 <chousik01@gmail.com>
2024-08-06 12:59:27 +0900

    수정

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,14 +4,14 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import {Input, InputSchema} from "@shared/input.schema"
-const url= 'mongodb+srv://chousik01:70318202@cluster0.mmiiheq.mongodb.net/'
+const url= 'mongodb+srv://chousik01:70318202@cluster0.mmiiheq.mongodb.net/testCho'
 
 
 
 @Module({
   imports: [
     MongooseModule.forRoot(url),
-    MongooseModule.forFeature([{ name: Input.name, schema: InputSchema }]),
+    MongooseModule.forFeature([{ name: Input.name, schema: InputSchema , collection : Input.name}]),
   ],
   controllers: [AppController],
   providers: [AppService],
