e7ef86de6f34dbee1427939de18a13812f537a46
황재민 <ghkdwoals1005@gmail.com>
2024-08-07 21:39:06 +0900

    Fix | backend/proxy.module.ts : Proxy.controller 제외, export에 ProxyService 추가

--- backend/src/proxy/proxy.module.ts
+++ backend/src/proxy/proxy.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
-import { ProxyController } from './proxy.controller';
 import { ProxyService } from './proxy.service';
 import { HttpModule } from '@nestjs/axios';
 
 @Module({
   imports: [HttpModule],
-  controllers: [ProxyController],
   providers: [ProxyService],
+  exports: [ProxyService],
 })
 export class ProxyModule {}
