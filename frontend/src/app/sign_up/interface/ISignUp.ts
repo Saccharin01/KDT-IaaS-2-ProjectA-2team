4c3bee24521a3ed70c175ddef85a98105e3188c1
조우식 <chousik01@gmail.com>
2024-08-07 18:01:03 +0900

    style | ISignUp.ts

--- frontend/src/app/sign_up/interface/ISignUp.ts
+++ frontend/src/app/sign_up/interface/ISignUp.ts
@@ -1,3 +1,3 @@
-import {IUserId, IPassword, INickName, IBudge} from "@shared/interface/userData"
+import {IUserId, IPassword, INickName, IBudget} from "@shared/interface/userData"
 
-export default interface IUserData extends IUserId, IPassword, INickName, IBudge {}
+export default interface IUserData extends IUserId, IPassword, INickName, IBudget {}
