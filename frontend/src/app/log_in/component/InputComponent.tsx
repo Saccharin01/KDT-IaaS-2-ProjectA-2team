9b407b9d606283b99a49f27ae3f974141b258e82
조우식 <chousik01@gmail.com>
2024-08-08 13:13:31 +0900

    new | InputComponent.tsx
    제발 단일책임원칙 제발

--- frontend/src/app/log_in/component/InputComponent.tsx
+++ frontend/src/app/log_in/component/InputComponent.tsx
@@ -1,24 +1,29 @@
 import React from "react"
 
 interface InputComponentInterface {
-  inputAmount: string[]; // 입력 필드의 이름을 담고 있는 배열
-  placeholderMsg: { [key: string]: string }; // 플레이스홀더 메시지를 담고 있는 객체
-  onChangeHandler: (event: React.ChangeEvent<HTMLInputElement>) => void; // 입력 필드의 값 변경을 처리하는 함수
+  key : number
+  name : string
+  placeholderMsg: string; // 플레이스홀더 메시지를 담고 있는 객체
+  onChangeHandler?: (event: React.ChangeEvent<HTMLInputElement>) => void; // 입력 필드의 값 변경을 처리하는 함수
   className?: string; // (선택 사항) 스타일 클래스를 적용할 수 있는 추가 prop
 }
 
+/**
+ * 
+ * @param param0 
+ * @returns 랜더링 하는 리액트 엘리먼트 반환
+ */
 
-const InputComponent :React.FC<InputComponentInterface> = ({ inputAmount, placeholderMsg, onChangeHandler, className }) => (
+const InputComponent :React.FC<InputComponentInterface> = ({key, placeholderMsg, onChangeHandler, name ,className}) => (
   <div>
-    {inputAmount.map((element, index) => (
-      <div key={index}>
+      <div key={key}>
         <input
           type="text"
-          placeholder={placeholderMsg[element]}
-          name={element}
+          placeholder={placeholderMsg}
+          name={name}
           onChange={onChangeHandler} 
           className={className} />
-      </div>))}
+      </div>)
   </div>
 )
 
