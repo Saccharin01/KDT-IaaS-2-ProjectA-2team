1c3617dd001f919d10c4841a79a8aae8a56c9179
황재민 <ghkdwoals1005@gmail.com>
2024-08-08 19:45:39 +0900

    Feat | search-service/search.service.ts : 쿼리문을 사용해 원하는 데이터(책 정보)를 가져오는 메소드 추가

--- search-service/src/search/search.service.ts
+++ search-service/src/search/search.service.ts
@@ -1,8 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { SearchResponse, BookDTO } from '@shared/SearchDTO';
-import { Book } from '@shared/schemas/book.schema';
+
+import {
+  BookSearchQuery,
+  BookSearchQueryFilter,
+} from '@shared/query/bookSearch.query';
+import { Book, BookDocument } from '@shared/schemas/book.schema';
 import { Model } from 'mongoose';
+import { ERR_MSG } from 'src/static/ERR_MSG';
 /**
  * search 컨트롤러에서 라우팅 된 메서드의 비즈니스 로직을 정의하는 서비스.
  * 컨트롤러에서 @query 데코레이터로 처리한 쿼리스트링의 값이 넘어오고, 해당 값을 인자로
@@ -11,31 +16,51 @@ import { Model } from 'mongoose';
 @Injectable()
 export class SearchService {
   constructor(
-    @InjectModel(Book.name) private readonly bookModel: Model<BookDTO>) {}
+    @InjectModel(Book.name) private readonly bookModel: Model<BookDocument>,
+  ) {}
 
-    searchBooksByTitle = async (query: string): Promise<SearchResponse> => {
-    const searchRegex = new RegExp(query, 'i'); // 대소문자 구분 없이 검색
+  // searchBooksByTitle = async (query: string): Promise<SearchResponse> => {
+  //   const searchRegex = new RegExp(query, 'i'); // 대소문자 구분 없이 검색
+
+  //   try {
+  //     const data = await this.bookModel
+  //       .find({ title: { $regex: searchRegex } })
+  //       .exec();
+  //     // console.log('Found data:', data);
+  //     return { incomeData: data };
+  //   } catch (err) {
+  //     console.error('Error fetching book by Title :', err);
+  //     throw err; // 에러 발생 시 프로미스를 reject 상태로 만듭니다.
+  //   }
+  // };
+
+  // searchBooksById = async (id: number): Promise<SearchResponse> => {
+  //   try {
+  //     // 숫자형 ID로 검색
+  //     const data = await this.bookModel.findById({ _id: id }).exec();
+  //     return { incomeData: data ? [data] : [] };
+  //   } catch (err) {
+  //     console.error('Error fetching book by ID :', err);
+  //     throw new Error(`Error fetching book by ID :${err}`);
+  //   }
+  // };
+
+  searchBooksByQuery(query: BookSearchQuery) {
+    const { page } = query;
+
+    //* 페이지 항목 수
+    const limit = 10;
+    const skip = (page - 1) * limit;
+
+    const filterObj = BookSearchQueryFilter(query);
 
     try {
-      const data = await this.bookModel.find({ title: { $regex: searchRegex } }).exec();
-      // console.log('Found data:', data);
-      return { incomeData: data };
-    } catch (err) {
-      console.error('Error fetching book by Title :', err);
-      throw err; // 에러 발생 시 프로미스를 reject 상태로 만듭니다.
-    }
-  };
-
-  searchBooksById = async(id: number): Promise<SearchResponse> => {
-    try {
-      // 숫자형 ID로 검색
-      const data = await this.bookModel.findById({ _id: id }).exec();
-      return { incomeData: data ? [data] : [] };
-    } catch (err) {
-      console.error('Error fetching book by ID :', err);
-      throw new Error(`Error fetching book by ID :${err}`);
+      return this.bookModel.find(filterObj).skip(skip).limit(limit).exec();
+    } catch (error) {
+      throw new HttpException(
+        ERR_MSG.SEARCH_ERROR,
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
     }
   }
-
-
 }
