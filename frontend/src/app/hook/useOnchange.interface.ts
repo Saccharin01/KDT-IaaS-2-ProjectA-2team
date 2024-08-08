9a47e5f34b5d338237290a4b1f3dd46fda4a8c4e
조우식 <chousik01@gmail.com>
2024-08-08 16:22:29 +0900

    new | useOnchange.interface.ts

---
+++ frontend/src/app/hook/useOnchange.interface.ts
@@ -0,0 +1,5 @@
+import {IBudget, INickName, IPassword, IUserId} from "@shared/interface/userData"
+
+interface useOnchangeInterface extends IBudget, INickName, IPassword, IUserId {}
+
+export default useOnchangeInterface
