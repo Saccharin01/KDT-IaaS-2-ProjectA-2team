61edf6584216d821e8b0839e3dcd2526bbaeae16
조우식 <chousik01@gmail.com>
2024-08-08 13:12:35 +0900

    feat | page.tsx
    스켈레톤 추가중

---
+++ frontend/src/app/log_in/page.tsx
@@ -0,0 +1,41 @@
+"use Client"
+
+import InputComponent from "./component/InputComponent";
+import { useState } from "react";
+
+
+
+
+
+
+interface LogInPageInterface {
+  somethingAwesome : string[]
+}
+
+
+const layoutData = {
+  userId : "아이디를 입력 해주세요",
+  password : "비밀번호를 입력 해주세요"
+}
+
+const LogInPage : React.FC<LogInPageInterface> = ({somethingAwesome}) => {
+  
+  const [value, setValue] = useState(
+    {
+      id : "",
+      password : ""
+    }
+  )
+
+  return(
+    somethingAwesome.map((elemet, index)=>
+    
+      <InputComponent
+      key={index}
+      name=""
+      placeholderMsg=""
+      onChangeHandler={console.log}
+      />
+    )
+  )
+}
