59b98411a4bbe999ed5e087c6f2ddc5de936d9ec
조우식 <chousik01@gmail.com>
2024-08-09 19:11:33 +0900

    feat | HTTP.ts
    결제 서비스 요청을 위해 HTTP 객체에 프로퍼티 추가

--- frontend/src/static/HTTP.ts
+++ frontend/src/static/HTTP.ts
@@ -1,8 +1,10 @@
 
 const MAIN_URL = process.env.NEXT_PUBLIC_PROXY_SERVICE_HOST
 const SEARCH_ROUTER = process.env.SEARCH_ROUTER
+const PAYMENT_ROUTER = process.env.PAYMENT_ROUTER
 
 export const HTTP = Object.freeze({
   HOST : MAIN_URL,
   SEARCH : SEARCH_ROUTER,
+  PAYMENT : PAYMENT_ROUTER,
 })
