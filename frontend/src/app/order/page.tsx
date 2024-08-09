bef7d746dc2151afae76e490f9e2d72600e2a0ae
조우식 <chousik01@gmail.com>
2024-08-09 16:00:15 +0900

    엑시오스 인스턴스로 바꿔야 할 거 같은데

--- frontend/src/app/order/page.tsx
+++ frontend/src/app/order/page.tsx
@@ -1,26 +1,71 @@
 "use client";
-import useOnchange from "./hooks/useOnchange/useOnchange";
+// import useOnchange from "./hooks/useOnchange/useOnchange";i
 import React from "react";
 import InputComponent from "./components/InputComponent";
+import axiosInstance from "frontend/src/module/axiosInstance";
+import { HTTP } from "../../../src/static/HTTP";
+import { useState } from "react";
+import PaymentDTO from "@shared/dto/paymentDTO"
 
 const placeholderMsg: { [key: string]: string } = {
-  user_id: "아이디를 입력하세요",
-  password: "비밀번호를 입력하세요",
-  nickName: "닉네임을 입력하세요",
-  budget: "예산이 어느정도 되시나요",
+  // user_id: "아이디를 입력하세요",
+  // nickName: "닉네임을 입력하세요",
+  // budget: "예산이 어느정도 되시나요",
+  address: "주소를 입력 해주세요",
+  price: "가격 입력",
+  amount: "수량 입력",
 };
 
 const tailwind = {
   button: "border-2 border-blue-700 m-4",
 };
 
-const LogIn: React.FC = () => {
-  const [inputValue, onChangeHandler] = useOnchange();
+
+
+const OrderSubmit: React.FC = () => {
+
+
+  const [inputValue, setInputValue] = useState<PaymentDTO>({
+    user_id: "",
+    book_id: 0,
+    address: "",
+    price: 0,
+    amount: 0,
+    payment : ""
+  });
+
+  const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    const { name, value } = e.target;
+    setInputValue((prevState) => {
+      let parsedValue;
+      if (name === "price" || name === "amount") {
+        parsedValue = parseInt(value);
+      } else if (name === "order_date") {
+        parsedValue = new Date(value);
+      } else {
+        parsedValue = value;
+      }
+
+      return {
+        ...prevState,
+        [name]: parsedValue,
+      };
+    });
+  };
+
+
+  axiosInstance.get(HTTP.SEARCH, {
+    params: {
+      ...result,
+      page: page,
+    },
+  });
+
 
   const ClickEvnet = async () => {
     console.log(inputValue);
     try {
-      const FetchData = await fetch(`http://localhost:3001/sign_up`, {
+      const FetchData = await fetch(`http://localhost:3000/payments`, {
         method: "POST",
         body: JSON.stringify(inputValue),
         headers: { "Content-Type": "application/json" },
@@ -44,14 +89,18 @@ const LogIn: React.FC = () => {
   return (
     <div>
       {inputAmount.map((element, index) => (
-        <InputComponent
-          type={element === "budget" ? "number" : "text"}
-          key={index}
-          name=""
-          placeholderMsg={placeholderMsg[element]}
-          onChange={onChangeHandler}
-          className=""
-        />
+        <div key={index}>
+          <InputComponent
+            key={index}
+            type={
+              element === "price" || element === "amount" ? "number" : "text"
+            }
+            name={element}
+            placeholderMsg={placeholderMsg[element]}
+            onChange={onChangeHandler}
+            className=""
+          />
+        </div>
       ))}
 
       <button type="button" onClick={ClickEvnet} className={tailwind.button}>
@@ -61,4 +110,4 @@ const LogIn: React.FC = () => {
   );
 };
 
-export default LogIn;
+export default OrderSubmit;
