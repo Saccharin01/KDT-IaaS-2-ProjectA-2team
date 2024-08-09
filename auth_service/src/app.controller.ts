03ef8547549ff27e6b63f9b7742af4296926ca79
황재민 <ghkdwoals1005@gmail.com>
2024-08-09 22:48:47 +0900

    Fix | auth-service/app.controller.ts : 회원가입 라우터, 매개변수 Dto 변경

--- auth_service/src/app.controller.ts
+++ auth_service/src/app.controller.ts
@@ -7,7 +7,8 @@ import {
   Get,
   Query,
 } from '@nestjs/common';
-import LoginDTO from '@shared/dto/loginDTO';
+import { LoginDto } from '@shared/dto/login.dto';
+import { AccountDto } from '@shared/dto/account.dto';
 import CheckIdDTO from '@shared/interfaces/checkId';
 import SuccessDTO from '@shared/dto/success.dto';
 import { AppService } from './app.service';
@@ -17,7 +18,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Post('login')
-  async login(@Body() loginDto: LoginDTO) {
+  async login(@Body() loginDto: LoginDto) {
     const user = await this.appService.validateUser(
       loginDto._id,
       loginDto.password,
@@ -32,7 +33,7 @@ export class AppController {
 
   // 회원가입 엔드포인트
   @Post('signup')
-  async signup(@Body() body: LoginDTO) {
+  async signup(@Body() body: AccountDto) {
     const result = await this.appService.create(body); // 사용자 생성
 
     if (result === false) throw new ConflictException('User already exists');
