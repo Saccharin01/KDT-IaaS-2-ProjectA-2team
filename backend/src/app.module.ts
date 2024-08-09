931de6230875dc3e3113e957cfc8ecbe2e7e5777
황재민 <ghkdwoals1005@gmail.com>
2024-08-09 22:50:17 +0900

    Feat | backend/app.module.ts : AuthModule Import

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,8 +3,14 @@ import { ConfigModule } from '@nestjs/config';
 import { configOptions } from '@shared/config/env.config';
 import { AdminModule } from './proxy-module/admin/admin.module';
 import { SearchModule } from './proxy-module/search/search.module';
+import { AuthModule } from './proxy-module/auth/auth.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(configOptions), AdminModule, SearchModule],
+  imports: [
+    ConfigModule.forRoot(configOptions),
+    AdminModule,
+    SearchModule,
+    AuthModule,
+  ],
 })
 export class AppModule {}
