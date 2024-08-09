18da9e5430ac0ca0a1f1cda7d0d61b790b3a8b5e
조우식 <chousik01@gmail.com>
2024-08-09 19:12:28 +0900

    모델추가

--- payment_service/src/payment/payment.module.ts
+++ payment_service/src/payment/payment.module.ts
@@ -1,16 +1,22 @@
 import { Module } from '@nestjs/common';
 import { PaymentController } from './payment.controller';
 import { PaymentService } from './payment.service';
-import { Payment, PaymentSchema } from '@shared/schemas/payment.schema';
 import { ConfigModule } from '@nestjs/config';
 import { configOptions } from '@shared/config/env.config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { Payment, PaymentSchema } from '@shared/schemas/payment.schema';
+import { Book, BookSchema } from '@shared/schemas/book.schema';
+import { User, UserSchema } from '@shared/schemas/user.schema';
 
 @Module({
   imports: [
     ConfigModule.forRoot(configOptions),
     MongooseModule.forRoot(process.env.DATABASE_HOST),
-    MongooseModule.forFeature([{ name: Payment.name, schema: PaymentSchema }]),
+    MongooseModule.forFeature([
+      { name: Payment.name, schema: PaymentSchema },
+      { name: Book.name, schema: BookSchema },
+      { name: User.name, schema: UserSchema },
+    ]),
   ],
 
   controllers: [PaymentController],
