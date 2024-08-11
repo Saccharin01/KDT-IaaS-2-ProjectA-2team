9abe7fb7264ac87518fb8517e5f00b5dc8a8bd2d
황재민 <ghkdwoals1005@gmail.com>
2024-08-11 23:06:00 +0900

    New | admin_service/payment.module.ts : payment에 대한 모듈

---
+++ admin_service/src/payments/payments.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { PaymentsController } from './payments.controller';
+import { PaymentsService } from './payments.service';
+import { MongooseModule} from '@nestjs/mongoose'
+import { PaymentSchema, Payment } from '@shared/schemas/payment.schema';
+
+@Module({
+  imports: [MongooseModule.forFeature([{name: Payment.name, schema: PaymentSchema}])],
+  controllers: [PaymentsController],
+  providers: [PaymentsService]
+})
+export class PaymentsModule {}
