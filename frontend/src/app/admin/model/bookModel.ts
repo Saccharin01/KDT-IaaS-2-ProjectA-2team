995170f5a18eed32508d5477dae90894cfc7d754
황재민 <ghkdwoals1005@gmail.com>
2024-08-11 22:26:12 +0900

    Fix | frontend/bookModel.ts : 테이블에서_id를 확인하기 위해 속성을 추가

--- frontend/src/app/admin/model/bookModel.ts
+++ frontend/src/app/admin/model/bookModel.ts
@@ -1,8 +1,9 @@
-import { ITableHeader } from "../components/table/interface/ITable";
+import { TableHeader } from "../components/table/interface/ITable";
 import { IFieldType } from "../components/table/interface/IField";
+import { BookDto } from "@shared/dto/book.dto";
 
-export const bookHeader: ITableHeader = {
-  //_id: ["Id", "range"],
+export const bookHeader: TableHeader<BookDto> = {
+  _id: ["Id", "range"],
   title: ["Title", "text"],
   author: ["Author", "text"],
   price: ["Price", "range"],
@@ -13,21 +14,19 @@ export const bookHeader: ITableHeader = {
   stock_quantity: ["Stock Quantity", "range"],
   arrival_date: ["Arrival Date", "text"],
   remain_stock: ["Remain Stock", "range"],
-  sold_stock: ["Sold Stock", "range"]
+  sold_stock: ["Sold Stock", "range"],
 };
 
 export const bookField: IFieldType = {
-  _id: 'none',
-  title: 'string',
-  author: 'string',
-  price: 'number',
-  genre: 'string',
-  publisher: 'string',
-  introduce: 'string',
-  hashtags: 'arr_string',
-  stock_quantity: 'number',
-  arrival_date: 'date',
-  remain_stock: 'number',
-  sold_stock: 'number'
+  title: "string",
+  author: "string",
+  price: "number",
+  genre: "string",
+  publisher: "string",
+  introduce: "string",
+  hashtags: "arr_string",
+  stock_quantity: "number",
+  arrival_date: "date",
+  remain_stock: "number",
+  sold_stock: "number",
 };
-
