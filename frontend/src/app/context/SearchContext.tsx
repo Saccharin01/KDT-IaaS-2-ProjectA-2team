81ec95a35d890d92305909404c3241ed07b2c61b
황재민 <ghkdwoals1005@gmail.com>
2024-08-11 22:13:56 +0900

    Feat | frontend/SearchContext.ts : 컨텍스트에 접근하기 위한 커스텀훅추가

--- frontend/src/app/context/SearchContext.tsx
+++ frontend/src/app/context/SearchContext.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { createContext, useState, ReactNode } from "react";
+import { createContext, useState, ReactNode, useContext } from "react";
 import { IBookSearchQuery, QUERY_TYPE } from "@shared/query/bookSearch.query";
 
 interface SearchContextType {
@@ -22,3 +22,13 @@ export const SearchProvider = ({ children }: { children: ReactNode }) => {
     </SearchContext.Provider>
   );
 };
+
+export const useSearch = () => {
+  const context = useContext(SearchContext);
+  
+  if(!context){
+    throw new Error("useSearch must be used within an searchProvider");
+  }
+
+  return context
+}
