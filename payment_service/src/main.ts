0247774ef6bdc42702fb789ba5a0cc1ab2d4aeed
황재민 <ghkdwoals1005@gmail.com>
2024-08-11 22:01:53 +0900

    New | payment_service/main.ts : 지정된 포트번호(3002) 및 프록시서버와 통신을 위해 CORS 허용

---
+++ payment_service/src/main.ts
@@ -0,0 +1,19 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
+import dotenv from 'dotenv';
+dotenv.config();
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const corsOptions: CorsOptions = {
+    origin: process.env.PROXY_SERVICE_HOST, // 허용할 도메인
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // 허용할 HTTP 메서드
+    credentials: true, // 쿠키를 허용하려면 true로 설정
+  };
+  app.enableCors(corsOptions);
+
+  await app.listen(3002);
+}
+bootstrap();
