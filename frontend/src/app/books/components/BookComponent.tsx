07c749b36340e95f5205eb7a14ef2836c0b50a38
황재민 <ghkdwoals1005@gmail.com>
2024-08-11 22:22:38 +0900

    Feat | frontend/BookComponent.tsx : 장바구니, 바로구매에 대한 버튼 이벤트 추가

--- frontend/src/app/books/components/BookComponent.tsx
+++ frontend/src/app/books/components/BookComponent.tsx
@@ -5,12 +5,23 @@ import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { handleTagClick } from "../../func/handleTagClick";
 import { QUERY_TYPE } from "@shared/query/bookSearch.query";
+import { useOrder } from "../../context/OrderContext";
+import { ROUTER_PATH } from "frontend/src/static/ROUTER_PATH";
 
 export default function BookComponent({
-...bockDTO
+...bookDTO
 }: BookDto) {
   const router = useRouter();
+  const {order, addOrder} = useOrder();
 
+  const addBookInOrder = () => {
+    addOrder(bookDTO)
+  }
+
+  const addBookAndMoveOrderPage = () => {
+    addBookInOrder();
+    router.push(ROUTER_PATH.CHECKOUT);
+  }
 
   return (
     <>
@@ -23,14 +34,14 @@ export default function BookComponent({
             height={150} // 이미지의 높이
           />
           <div className="ml-4 flex flex-col overflow-hidden">
-            <h2 className="text-lg font-bold mt-2">{bockDTO.title}</h2>
-            <p>{bockDTO.author}</p>
+            <h2 className="text-lg font-bold mt-2">{bookDTO.title}</h2>
+            <p>{bookDTO.author}</p>
             <p className="text-green-600 font-bold mt-2">
-              {bockDTO.price}
+              {bookDTO.price}
               {/* 10% 12,600원 <span className="line-through">{price}</span> */}
             </p>
             <div className="mt-2 space-x-2">
-              {bockDTO.hashtags.map((value: string, index) => (
+              {bookDTO.hashtags.map((value: string, index) => (
                 <span key={index} className="px-2 py-1 text-violet-600 border border-violet-600 hover:bg-violet-600 hover:text-white active:bg-indigo-500 focus:outline-none focus:ring rounded" onClick={() => {handleTagClick(QUERY_TYPE.TAG,value, router)}}>
                   {value}
                 </span>
@@ -39,10 +50,10 @@ export default function BookComponent({
           </div>
         </div>
         <div className="flex flex-col space-y-2">
-          <button className="px-4 py-2 bg-gray-200 text-gray-800 rounded">
+          <button className="px-4 py-2 bg-gray-200 text-gray-800 rounded" onClick={addBookInOrder}>
             장바구니
           </button>
-          <button className="px-4 py-2 bg-blue-600 text-white rounded">
+          <button className="px-4 py-2 bg-blue-600 text-white rounded" onClick={addBookAndMoveOrderPage}>
             바로구매
           </button>
         </div>
