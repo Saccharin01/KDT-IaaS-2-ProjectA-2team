f5491d6f52a9344f7298161a3265f7ab1af35fbb
황재민 <ghkdwoals1005@gmail.com>
2024-08-11 22:11:34 +0900

    Feat | frontend/next.config.mjs : 테스트를 위한 외부이미지 경로, payment_service의 경로 환경변수 추가

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -7,11 +7,15 @@ dotenv.config({
 
 const nextConfig = {
   reactStrictMode: false,
+  images: {
+    domains: ['images.unsplash.com'], // 외부 이미지 로드가 허용될 도메인을 추가합니다.
+  },
   env: {
     NEXT_PUBLIC_PROXY_SERVICE_HOST: process.env.PROXY_SERVICE_HOST,
     SEARCH_ROUTER: process.env.SEARCH_ROUTER,
     AUTH_SIGNUP_ROUTER: process.env.AUTH_SIGNUP_ROUTER,
-    AUTH_LOGIN_ROUTER: process.env.AUTH_LOGIN_ROUTER
+    AUTH_LOGIN_ROUTER: process.env.AUTH_LOGIN_ROUTER,
+    PAYMENT_PURCHASE_ROUTER: process.env.PAYMENT_PURCHASE_ROUTER
   },
 };
 
