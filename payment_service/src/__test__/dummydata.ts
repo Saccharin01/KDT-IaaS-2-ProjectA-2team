ebd560f6f31819c29f3218388b8d8c7447f1e08b
황재민 <ghkdwoals1005@gmail.com>
2024-08-11 22:05:09 +0900

    Test | New | payment_servie/dummydata.ts : 테스트를 위한 더미데이터 입니다

---
+++ payment_service/src/__test__/dummydata.ts
@@ -0,0 +1,27 @@
+import { PaymentDto } from '@shared/dto/payment.dto';
+import { BookDto } from '@shared/dto/book.dto';
+
+export const dummyPaymentDto: PaymentDto = {
+  book_id: 1,
+  user_id: '안녕',
+  price: 22000,
+  address: 'test',
+  payment: 'test',
+  amount: 1,
+  date: new Date().toISOString(),
+};
+
+export const dummyBook = {
+  _id: 1,
+  title: 'The Great Gatsby',
+  author: 'F. Scott Fitzgerald',
+  price: 10.99,
+  genre: 'Fiction',
+  publisher: 'Scribner',
+  stock_quantity: 100,
+  introduce: 'A novel set in the Jazz Age on Long Island.',
+  remain_stock: 100,
+  sold_stock: 0,
+  arrival_date: new Date(),
+  hashtags: ['init'],
+}
