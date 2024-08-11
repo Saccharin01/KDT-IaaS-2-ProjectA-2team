3ee0cc37bf22d62ba7e03f7ffd19677f6e5ba6c3
황재민 <ghkdwoals1005@gmail.com>
2024-08-11 22:34:47 +0900

    Style | frontend/button.tsx : 너비 조정

--- frontend/src/app/admin/components/button.tsx
+++ frontend/src/app/admin/components/button.tsx
@@ -7,7 +7,7 @@ export function ButtonComponent({name, func} : IButtonProps) {
   return (
     <label className="cursor-pointer">
       <input type="radio" className="peer sr-only" name="pricing" onClick={func} />
-      <div className="w-72 max-w-xl rounded-md bg-white p-5 text-gray-600 ring-2 ring-transparent transition-all hover:shadow peer-checked:text-sky-600 peer-checked:ring-blue-400 peer-checked:ring-offset-2">
+      <div className="w-full max-w-xl rounded-md bg-white p-5 text-gray-600 ring-2 ring-transparent transition-all hover:shadow peer-checked:text-sky-600 peer-checked:ring-blue-400 peer-checked:ring-offset-2">
         <div className="flex flex-col gap-1">
           <div className="flex items-center justify-between">
             <p className="text-sm font-semibold uppercase text-gray-500">
