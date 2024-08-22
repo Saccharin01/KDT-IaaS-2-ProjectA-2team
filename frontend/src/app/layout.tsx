b87782c84fea2fa16e0da4ed855c025240208619
jaemin1005 <ghkdwoals1005@gmail.com>
2024-08-22 14:42:23 +0900

    Style | layout.tsx : 루트 레이아웃의 주석 추가

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -8,15 +8,28 @@ import { OrderProvider } from "./context/OrderContext";
 
 const inter = Inter({ subsets: ["latin"] });
 
+/**
+ * * 황재민
+ * * metadata => Header mete 정보를 기록
+ */
 export const metadata: Metadata = {
   title: "CyberFunc Book",
   description: "A simple bookstore app",
 };
+
+/**
+ * * 황재민
+ * * 페이지의 전체적인 레이아웃을 나타낸다.
+ * * 크게 두가지의 영역으로 구분 <Header, main> => < Navbar, children(page.tsx) >
+ * @param param0 : page.tsx  
+ * @returns 
+ */
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  //* Provider를 최상단 루트에 넣어둔 이유, 유저 정보, 주문 목록 활용하기 위해서
   return (
     <html lang="en">
       <UserProvider>
