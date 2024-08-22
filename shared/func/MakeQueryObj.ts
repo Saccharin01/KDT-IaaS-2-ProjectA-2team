f8a52738bf4af1b7d6f2a684f502498c0de02d51
jaemin1005 <ghkdwoals1005@gmail.com>
2024-08-22 14:50:08 +0900

    Style | MakeQueryObj.ts : 주석을 추가하였습니다

--- shared/func/MakeQueryObj.ts
+++ shared/func/MakeQueryObj.ts
@@ -1,11 +1,19 @@
 import {ReadonlyURLSearchParams} from "next/navigation";
 import { BookSearchQuery, IsSearchType, QUERY_KEY } from "@shared/query/bookSearch.query";
 
+/**
+ * * 황재민
+ * * 목표 : 쿼리스트링과 페이지를 통해 서비스에 보내는 body를 구성한다.
+ * @param params : Query
+ * @param page : 현재 페이지
+ * @returns : request.body
+ */
 export function MakeQueryObj(params: ReadonlyURLSearchParams, page: number): BookSearchQuery | null {
   const type = params.get(QUERY_KEY.TYPE);
   const content = params.get(QUERY_KEY.CONTENT);
 
   if(!IsSearchType(type)) return null;
 
+  //* 객체 반환
   return new BookSearchQuery(type, content, page)
 }
