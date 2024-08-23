58cbe98e307b6ca2a88bac88a5e7a8b5b56ac3ca
jaemin1005 <ghkdwoals1005@gmail.com>
2024-08-23 16:20:43 +0900

    Styles | ITable.ts : 주석 수정

--- frontend/src/app/admin/components/table/interface/ITable.ts
+++ frontend/src/app/admin/components/table/interface/ITable.ts
@@ -7,6 +7,6 @@ export interface ITableHeader {
 /**
  * * 황재민
  * * T는 객체가 들어온다.
- * * TableHeader는 [객체의 키(ㅅ)]
+ * * TableHeader는 [객체의 키(T)] : [string(테이블 헤더이름), 적용할 필터]
  */
 export type TableHeader<T> = Partial<Record<keyof T, [string, HeaderFilter]>>
