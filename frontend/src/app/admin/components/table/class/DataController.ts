fb48dae34be99a9797ace6f7d228445334cf8f54
jaemin1005 <ghkdwoals1005@gmail.com>
2024-08-23 16:17:37 +0900

    Styles | DataController.ts : 주석 추가

--- frontend/src/app/admin/components/table/class/DataController.ts
+++ frontend/src/app/admin/components/table/class/DataController.ts
@@ -5,8 +5,16 @@ export class DataController {
   private post: string
   private put: string
   
+  /**
+   * * 황재민
+   * TODO 삭제에 대한 엔드포인트가 존재하지 않는다.
+   * @param baseURL 서비스 주소
+   * @param post 엔드 포인트(post)
+   * @param put 엔드 포인트(put)
+   */
   constructor(baseURL: string, post: string, put: string){
 
+    //* axiosInstance를 만든다
     this.axiosInstance = axios.create({
       baseURL,
       headers : {
