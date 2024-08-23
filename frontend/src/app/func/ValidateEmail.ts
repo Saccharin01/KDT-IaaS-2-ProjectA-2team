d32a19c21ec43d5669e00c75af9489cc1ba7e215
jaemin1005 <ghkdwoals1005@gmail.com>
2024-08-23 16:23:57 +0900

    Styles | ValidateEmail.ts : 주석 추가

--- frontend/src/app/func/ValidateEmail.ts
+++ frontend/src/app/func/ValidateEmail.ts
@@ -1,3 +1,9 @@
+/**
+ * * 황재민
+ * * 정규식을 통해 이메일 검증
+ * @param email 
+ * @returns 
+ */
 export function ValidateEmail(email: string): boolean {
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   return emailRegex.test(email);
